22158816322a933e0abb1d5e8a65cfdf14f0870e
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-11 18:59:54 -0500

    Crear middlewares para la autenticación

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const services = require("../services");
+
+function isAuth(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(403).send({ message: "No tienes autorizacion" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  services
+    .decodeToken(token)
+    .then((response) => {
+      req.user = response;
+      next();
+    })
+    .catch((response) => {
+      res.status(response.status);
+    });
+}
+
+module.exports = { isAuth };
