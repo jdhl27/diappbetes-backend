bc68042f83625b67077af0ff57d445caf9dca966
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-12 08:23:36 -0500

    Agregar base de datos remota

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb+srv://jdhl27:tCke6MyoEXqHxpKP@cluster0.wrmrdts.mongodb.net/?retryWrites=true&w=majority',
+    db: process.env.MONGODB,
     dbLocal: process.env.MONGODB || 'mongodb://localhost:27017/diappbetes',
     SECRET_TOKEN: 'miclavedetokens'
 }
