6601688c2a1dbfc21eccfa08ffb2a34a2dbe788a
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-11 19:00:31 -0500

    Creando base de rutas

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const auth = require("../middlewares/auth");
+const userControl = require('../controllers/user')
+const api = express.Router();
+
+api.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+api.post("/signup", userControl.signUp);
+api.post("/signIn", userControl.signIn);
+
+api.get("/users", auth.isAuth, function (req, res) {
+  res.status(200).send({ message: "Lista de usuarios con acceso" });
+});
+
+// app.get("/hola/:name", function (req, res) {
+//   res.send({ message: `Hello ${req.params.name}` });
+// });
+
+module.exports = api;
