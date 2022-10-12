631677628978e192fb6055c5fb0f77f5bd7f341c
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-11 19:00:10 -0500

    Crear controlador de usuario

---
+++ src/controllers/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const service = require("../services");
+
+function signUp(req, res) {
+  const user = new User({
+    email: req.body.email,
+    displayName: req.body.displayName,
+  });
+
+  user.save((err) => {
+    if (err) res.status(500).send({ message: `Error al crear el usuario` });
+
+    return res.status(200).send({ token: service.createToken(user) });
+  });
+}
+
+function signIn(req, res) {
+  User.find({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send({ message: err });
+
+    if (!user) return res.status(404).send({ message: "No existe el usuario" });
+
+    req.user = user;
+    res.status(200).send({
+      message: "Te has logueado correctamente",
+      token: service.createToken(user),
+    });
+  });
+}
+
+module.exports = {
+  signUp,
+  signIn,
+};
