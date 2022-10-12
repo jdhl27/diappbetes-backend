066c8db44da164bd1ef452892e8bc77294cafe13
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-11 20:35:03 -0500

    Base de datos remota

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/diappbetes',
+    db: process.env.MONGODB || 'mongodb+srv://jdhl27:tCke6MyoEXqHxpKP@cluster0.wrmrdts.mongodb.net/?retryWrites=true&w=majority',
+    dbLocal: process.env.MONGODB || 'mongodb://localhost:27017/diappbetes',
     SECRET_TOKEN: 'miclavedetokens'
 }
