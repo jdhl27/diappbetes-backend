096f3f0b60fec546e7c8da8af1fe854c417a511c
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-10-12 23:01:25 -0500

    Autorizar cors para peticiones

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const api = require("./routes");
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api", api);
