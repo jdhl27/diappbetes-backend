cd1a6cc0c0e29567db119424ae4312b58a2a39ad
Juan Hernandez <independientemedellin.27.03@gmail.com>
2022-11-14 23:23:02 -0500

    Archivos dinámicos

--- src/middlewares/utils.js
+++ src/middlewares/utils.js
@@ -1,10 +1,19 @@
 function generatePDF() {
   const num = Math.floor(Math.random() * 4);
   let array = [];
+  const names = [
+    "formula_julio.pdf",
+    "recomendaciones1.pdf",
+    "examenes.pdf",
+    "importante.pdf",
+    "formula_medica.pdf",
+    "tenerencuenta.pdf",
+  ];
   for (let i = 0; i < num; i++) {
-    array.push(
-      "http://jornadasciberseguridad.riasc.unileon.es/archivos/ejemplo_esp.pdf"
-    );
+    array.push({
+      name: names[Math.floor(Math.random() * names.length)],
+      url: "http://jornadasciberseguridad.riasc.unileon.es/archivos/ejemplo_esp.pdf",
+    });
   }
   return array;
 }
